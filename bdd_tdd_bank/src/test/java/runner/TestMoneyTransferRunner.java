f0fcacaae846a0749ec594deb04f601ded88bfcc
Themis <themisgcp@gmail.com>
2023-11-29 16:56:52 +0100

    update

--- bdd_tdd_bank/src/test/java/runner/TestMoneyTransferRunner.java
+++ bdd_tdd_bank/src/test/java/runner/TestMoneyTransferRunner.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features/MoneyTransfer.feature",
-        glue = "steps",
-        publish = true
+        glue = "steps"
+        //publish = true
 )
 public class TestMoneyTransferRunner {
 }
